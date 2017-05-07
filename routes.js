ade0439e0eb99e7e93dfeb634744566476a98460
Jae Huh <jaeeunhuh@gmail.com>
2017-05-07 15:59:15 +1200

    Organise hbs and css files

--- routes.js
+++ routes.js
@@ -3,6 +3,30 @@ const router = express.Router()
 
 module.exports = router
 
+const links = {
+  home: '/',
+  about: '/about',
+  askQuestions: '/askQuestions',
+  viewQuestions: '/viewQuestions',
+  viewAndVote: '/viewAndVote'
+}
+
 router.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', links)
+})
+
+router.get('/about', (req, res) => {
+  res.render('about', links)
+})
+
+router.get('/askQuestions', (req, res) => {
+  res.render('askQuestions', links)
+})
+
+router.get('/viewQuestions', (req, res) => {
+  res.render('viewQuestions', links)
+})
+
+router.get('/viewAndVote', (req, res) => {
+  res.render('viewAndVote', links)
 })
