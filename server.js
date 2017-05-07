56c3a3928d017aeac00f8f7852e9f0ecddf5b224
Jae Huh <jaeeunhuh@gmail.com>
2017-05-07 12:41:41 +1200

    Basic setup for server to run

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
-express
-express-handlebars
-body-parser
+const express = require('express')
+const hbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const routes = require('./routes')
+
+const app = express()
+
+const hbsConfig = {
+  extname: 'hbs',
+  defaultLayouts: 'main'
+}
+
+app.engine('hbs', hbs(hbsConfig))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use('/', routes)
+
+module.exports = app
