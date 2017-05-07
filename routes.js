ed410e361d11af817bc68895ef7322472435783a
Jae Huh <jaeeunhuh@gmail.com>
2017-05-07 17:25:05 +1200

    Create data as JSON

--- routes.js
+++ routes.js
@@ -1,18 +1,30 @@
 const express = require('express')
 const router = express.Router()
+const fs = require('fs')
 
 module.exports = router
 
-const links = {
-  home: '/',
-  about: '/about',
-  askQuestions: '/askQuestions',
-  viewQuestions: '/viewQuestions',
-  viewAndVote: '/viewAndVote'
+const data = require('./data.json')
+
+function getFileContents (file, callback) {
+  fs.readFile(file, 'utf-8', (err, contents) => {
+    if (err) {
+      callback(err)
+    }
+    const data = JSON.parse(contents)
+    callback(null, data)
+  })
 }
 
+
+
 router.get('/', (req, res) => {
-  res.render('index', links)
+  getFileContents((data, contents) => {
+    if (err) {
+      return err
+    }
+    res.render('index', data)
+  })
 })
 
 router.get('/about', (req, res) => {
@@ -23,10 +35,17 @@ router.get('/askQuestions', (req, res) => {
   res.render('askQuestions', links)
 })
 
+
+// Need to edit post link to add the question
+router.post('/viewAndVote', (req, res) => {
+  res.render('viewAndVote', req.body)
+})
+
 router.get('/viewQuestions', (req, res) => {
   res.render('viewQuestions', links)
 })
 
+// Need to edit post link to add the question
 router.get('/viewAndVote', (req, res) => {
   res.render('viewAndVote', links)
 })
